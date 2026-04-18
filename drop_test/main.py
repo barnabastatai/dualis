4112202ae1c272b0de5f113ff132270da4fa8e83
Tatai Barnabás <tatai.barnabas@njit.hu>
2026-04-19 01:34:22 +0200

    drop test

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,9 +1,30 @@
 from pathlib import Path
 
+def min_num_of_drops(n, h):
+    if h <= 1:
+        return h
+
+    if n == 1:
+        return h
+
+    matrix = [[0] * (h + 1) for _ in range(n + 1)]
+
+    for i in range(1, h + 1):
+        for j in range(1, n + 1):
+            matrix[j][i] = matrix[j - 1][i - 1] + matrix[j][i - 1] + 1
+
+        if matrix[n][i] >= h:
+            return i
+
+    return h
+
 
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    data = Path("input.txt").read_text(encoding="utf-8").splitlines()
+
+    for line in data:
+        sp = line.replace(" ", "").split(",")
+        print(f"min_num_of_drops({sp[0]}, {sp[1]}) => {min_num_of_drops(int(sp[0]), int(sp[1]))}")
 
 
 if __name__ == "__main__":
